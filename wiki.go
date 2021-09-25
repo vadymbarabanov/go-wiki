e4a30ffb3a972fc59ec3181ed53300dd4152b212
Vadym Barabanov <vadym@pop-os.localdomain>
2021-09-25 18:23:40 +0300

    Go Wiki implementation

---
+++ wiki.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"text/template"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+var (
+	templates  = template.Must(template.ParseFiles("templates/edit.html", "templates/view.html"))
+	validPath  = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+	linkRegExp = regexp.MustCompile("\\[[a-zA-Z]+\\]")
+)
+
+func main() {
+	http.HandleFunc("/", redirectToHomePage)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+func redirectToHomePage(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view/home", http.StatusFound)
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalid Page Title")
+	}
+	return m[2], nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+
+	p := &Page{Title: title, Body: implementLinking([]byte(body))}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile("data/"+filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile("data/" + filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func implementLinking(body []byte) []byte {
+	return linkRegExp.ReplaceAllFunc(body, func(s []byte) []byte {
+		m := string(s[1 : len(s)-1])
+		return []byte("<a href=\"/view/" + m + "\">" + m + "</a>")
+	})
+}
